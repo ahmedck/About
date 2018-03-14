b79a62b152559a1877ba5e5970dca8e572811582
Ahmed Cherif KRID <a.cherifkrid@openbee.com>
2018-03-14 09:43:07 +0100

    change titre v1.1

--- About.js
+++ About.js
@@ -5,7 +5,7 @@ class About extends Component {
     render() {
         return (
             <div>
-                <h1>About</h1>
+                <h1>About Us</h1>
 
             </div>
         );
